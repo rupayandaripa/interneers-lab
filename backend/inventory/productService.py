e83d6e423cd57f8e4f52f96b4699b3b8ba05c071
Rupayan Daripa <d.rupayan@iitg.ac.in>
2025-04-02 12:13:24 +0530

    Merge conflicts resolved

---
+++ backend/inventory/productService.py
@@ -0,0 +1,36 @@
+from .models import ProductRepository
+
+class ProductServices:
+    @staticmethod   
+    def fetch_all_products(page_number):
+        # print(ProductRepository.__dict__.get("objects"))  # Check if it's overridden
+        # print(ProductRepository.objects.all())  # Should return a QuerySet
+        return ProductRepository.objects.fetch_all_products(page_number)
+    
+    @staticmethod
+    def get_product_by_id(product_id):
+        return ProductRepository.objects.get_product_by_id(product_id=product_id)
+    
+    @staticmethod
+    def get_product_by_brand(brand_name):
+        return ProductRepository.objects.get_product_by_brand(brand_name)
+    
+    @staticmethod
+    def add_product(name, description, category, price, brand, quantity):
+        return ProductRepository.add_product(
+            name=name, 
+            description=description, 
+            category=category, 
+            price=price, 
+            brand=brand, 
+            quantity=quantity
+        )
+    
+    @staticmethod
+    def update_product(product_id, data):
+        return ProductRepository.objects.update_product(product_id, data)
+    
+    @staticmethod
+    def delete_product(product_id):
+        return ProductRepository.objects.delete_product(product_id)
+        
---
+++ backend/manage.py
@@ -0,0 +1,61 @@
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+from datetime import datetime , timezone
+from mongoengine import connect
+from inventory.models import ProductRepository , Category
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+import django
+django.setup()
+
+
+
+def migrate_product_categories():
+    print("Starting product category migration....")
+    
+    connect('Inventory' , host='localhost' , port=27017)
+    print("Database connected successfully.........")
+    Category.seed_categories()
+    print("Categories seeded")
+    
+    products = ProductRepository.objects()
+    for product in products:
+        if isinstance(product.category , list) and all(isinstance(cat , Category) for cat in product.category):
+            print(f"Skipping product {product.product_id} (already migrated)")
+            continue
+        
+        if isinstance(product.category , str):
+            category_ref = Category.objects(category = product.category).first()
+            
+        if category_ref:
+            product.category = [category_ref]
+        else :
+            product.category = []
+            
+        product.updated_at = datetime.now(timezone.utc)
+        
+        try:
+            product.save()
+        except Exception as e:
+            print(f"Failed to migrate product {product.product_id}: {e}")
+            
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    migrate_product_categories() 
+    main()
