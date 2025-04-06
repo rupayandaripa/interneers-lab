9cc0ac57fb98b34765b752a34873cacdee5ef0b9
Rupayan Daripa <d.rupayan@iitg.ac.in>
2025-04-06 16:45:27 +0530

    Basic frontend using html css and javascript

--- backend/django_app/settings.py
+++ backend/django_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "inventory",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "django_app.urls"
@@ -122,3 +124,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
---
+++ frontend/product_cards/product_cards.js
@@ -0,0 +1,43 @@
+const url = 'http://127.0.0.1:8000/products/1/'
+
+async function fetchProductData() {
+    try {
+        const response = await fetch(url)
+
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        // document.querySelector(".product-name").textContent = data['product']['name']
+        // document.querySelector('.product-description').textContent = data['product']['description']
+        // document.querySelector('.product-price').textContent = `$ ${data['product']['price']}`
+        // document.querySelector('.product-category').textContent = data['product']['category']
+        // document.querySelector('.product-brand').textContent = data['product']['brand']
+
+        const productList = document.getElementById('product-list')
+        productList.innerHTML = ""
+
+        data.products.forEach(product => {
+            const productTile = document.createElement("div")
+            productTile.classList.add("product-tile")
+
+            productTile.innerHTML = `
+                <h2 class='product-name'>${product['name']}</h2>
+                <p class="product-description">${product.description}</p>
+                <p class="product-price">$${product.price}</p>
+                <p class="product-category">Category: ${product.category}</p>
+                <p class="product-brand">Brand: ${product.brand}</p>
+                `
+
+            productList.appendChild(productTile)
+        })
+    }
+    catch (error) {
+        console.error("Error fetching product data:", error)
+    }
+}
+
+fetchProductData()
+
